6b12e8c8d1d7f7c59b205fe3fad268789945a778
rohachuk <a.rohachuk@evnedev.com>
2022-07-18 21:19:29 +0300

    some UI changes

--- test/controllers/spends_controller_test.rb
+++ test/controllers/spends_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class SpendsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   # test "the truth" do
   #   assert true
   # end
+  test "get index" do
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+    sign_in users(:bob)
+    get :index
+    assert_response :ok
+  end
 end
